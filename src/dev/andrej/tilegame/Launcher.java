06c7516053e18828b7fe3e6fa40d4d16111ad812
Andrejob99 <andrejobsteinbeck@gmail.com>
2023-11-08 13:34:49 +0100

    Initial commit

---
+++ src/dev/andrej/tilegame/Launcher.java
@@ -0,0 +1,13 @@
+package dev.andrej.tilegame;
+
+import dev.andrej.tilegame.display.Display;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+        new Game("Lil Bro", 400, 600);
+
+    }
+
+}
