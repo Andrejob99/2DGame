5c770852db16be48343a8fd99577d115e633a185
Andrejob99 <andrejobsteinbeck@gmail.com>
2023-12-23 17:47:11 +0100

    added entity manager to tick and render all entities, including the player

--- src/dev/andrej/tilegame/states/GameState.java
+++ src/dev/andrej/tilegame/states/GameState.java
@@ -3,6 +3,7 @@ package dev.andrej.tilegame.states;
 import dev.andrej.tilegame.Game;
 import dev.andrej.tilegame.Handler;
 import dev.andrej.tilegame.entities.creatures.Player;
+import dev.andrej.tilegame.entities.creatures.statics.Tree;
 import dev.andrej.tilegame.gfx.Assets;
 import dev.andrej.tilegame.tiles.Tile;
 import dev.andrej.tilegame.worlds.World;
@@ -11,25 +12,21 @@ import java.awt.*;
 
 public class GameState extends State {
 
-    private Player player;
     private World world;
 
     public GameState(Handler handler) {
         super(handler);
         world = new World(handler,"res/worlds/world1.txt");
         handler.setWorld(world);
-        player = new Player(handler,world.getSpawnX()*64,world.getSpawnY()*64);
     }
 
     @Override
     public void tick() {
         world.tick();
-        player.tick();
     }
 
     @Override
     public void render(Graphics g) {
         world.render(g);
-        player.render(g);
     }
 }
